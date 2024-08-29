2367fb1878fb391812cf9220536d3712cbdb7474
PanneerSelvamR <panneer80@gmail.com>
2024-08-29 14:37:15 +0530

    test changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
 st.title("Hello World")
-
-st.text_input("Movie title", "Life of Brian")
 with st.sidebar:
     st.header("Side Heading")
     st.write("some random text")
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+with tab1:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.radio("Select one:", [1, 2])
+
+with tab2:
+    expand = st.expander("My label", icon=":material/info:")
+    expand.write("Inside the expander.")
+    pop = st.popover("Button label")
+    pop.checkbox("Show all")
+
+
+
+
 
