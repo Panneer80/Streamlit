3b64137ca61371930ed695dbd55f2e2d41c69370
PanneerSelvamR <panneer80@gmail.com>
2024-09-15 17:05:11 +0530

    db and config files added

--- modules/task_manager/task_manager.py
+++ modules/task_manager/task_manager.py
@@ -1,8 +1,29 @@
 import streamlit as st
 import pandas as pd
+from sqlalchemy.sql import text
+import json
 
 st.text("Task List")
-list_data = pd.read_json("modules/task_manager/task_data.json")
+file = open("modules/task_manager/task_data.json", 'r')
+list_data = json.load(file)
 selected_coloums = ['id','title','assigned to']
 
-st.dataframe(list_data[selected_coloums].set_index('id'))
+#st.dataframe(list_data[selected_coloums].set_index('id'))
+
+conn = st.connection('tasks_db', type='sql')
+
+# Insert some data with conn.session.
+with conn.session as s:
+    s.execute(text("CREATE TABLE IF NOT EXISTS task_main (id INTEGER, Title TEXT, Description TEXT, Assigned_To TEXT, Status TEXT);"))
+    #s.execute('DELETE FROM pet_owners;')
+    #task_data = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
+    for k in list_data:
+        s.execute(
+            text('INSERT INTO task_main (Title, Description,Assigned_To,Status ) VALUES (:title, :description, :assignedto, :status);'),
+            params=dict(title=k['title'], description=k['description'], assignedto=k['assigned to'], status='new')
+        )
+    s.commit()
+
+# Query and display the data you inserted
+task_main_df = conn.query('select * from task_main')
+st.dataframe(task_main_df)
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,13 @@ import streamlit as st
 #import dependent packages
 from learning import basics, widgets_tut, media, plotting, file_process,file_download
 
-st.set_page_config(page_title="Developer: Panneer Selvam R 🛠️", 
+st.set_page_config(page_title="Developer: Panneer Selvam R", 
                    page_icon="🐺",
                    initial_sidebar_state='auto',
                 layout='centered'
                    )
 
 def main():
-    # basics.basics_text()
-    # widgets_tut.widget_t()
-    # media.media_t()
-    #plotting.charts_t()
-    #file_process.file_manager()
-    # file_download.main()
-    # st.title("Welcome!")
-
     if "role" not in st.session_state:
         st.session_state.role = None
 
@@ -53,31 +45,18 @@ def main():
     )
 
     profile = st.Page(
-        "modules/about_me.py",
+        "modules/about_me/about_me.py",
         title="Profile",
         icon=":material/person_add:",
         url_path="admin", 
         default=True
     )
 
-    # st.logo("🐺")
+    # st.logo("modules/about_me/Photo_Panneer.jpg")
     page_dict = {}
     page_dict["About Me"] = [profile]
     page_dict["My Apps"] = [task_manager]
-
-    # page_dict = {}
-    # if st.session_state.role in ["admin"]:
-    #     page_dict["Admin"] = [admin]
-    # if st.session_state.role in ["user"]:
-    #     page_dict["Home"] = [home]
-
     pg = st.navigation(page_dict )
-    # if len(page_dict) > 0:
-    #     pg = st.navigation(page_dict | {"Log out": [logout_page]}  )
-    # else:
-    #     pg = st.navigation([st.Page(login)])
-        
-
     pg.run()
 
 if __name__ == '__main__':
