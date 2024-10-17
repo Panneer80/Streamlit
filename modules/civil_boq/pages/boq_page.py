3f4c94223029fb5a79db0f4fd39b57a14d5a5a13
PanneerSelvamR <panneer80@gmail.com>
2024-10-17 17:40:31 +0530

    fixed navigation issue

--- modules/civil_boq/pages/boq_page.py
+++ modules/civil_boq/pages/boq_page.py
@@ -6,7 +6,7 @@ from modules.civil_boq.page_elements.doors_windows import door_window_work
 from modules.civil_boq.page_elements.masonary_work import masonary_work
 from modules.civil_boq.page_elements.price_setting import set_price
 from modules.civil_boq.page_elements.tiles_work import tiles_work
-st.write(st.session_state)
+# st.write(st.session_state)
 st.header("BOQ Generator")
 st.markdown("##### Enter your data")
 st.divider()
@@ -20,5 +20,5 @@ st.divider()
 generate, print, clear, finalize = st.columns(4)
  
 if st.button("Generate BOQ", type="primary", key="generate_btn_poq_page_end"):
-    st.switch_page("modules/civil_boq/print_page.py")
+    st.switch_page("modules/civil_boq/pages/print_page.py")
 
